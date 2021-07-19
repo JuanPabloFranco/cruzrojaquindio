c1ede30843232ea352b0e7d5c55c03de765d8adc
JuanPabloFranco <juan.franco@igniweb.com>
2021-07-18 22:37:44 -0500

    Cambio slide cruz roja

---
+++ slidecruzroja.php
@@ -0,0 +1,200 @@
+<div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+    <!--Indicators-->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-1z" data-slide-to="1"></li>
+        <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+        <li data-target="#carousel-example-1z" data-slide-to="3"></li>
+        <li data-target="#carousel-example-1z" data-slide-to="4"></li>
+        <li data-target="#carousel-example-1z" data-slide-to="5"></li>
+        <li data-target="#carousel-example-1z" data-slide-to="6"></li>
+    </ol>
+    <!--/.Indicators-->
+    <!--Slides-->
+    <div class="carousel-inner" role="listbox">
+        <!--Second slide-->
+        <div class="carousel-item">
+            <div class="view">
+                <!-- Mask & flexbox options-->
+                <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+                    <!-- Content -->
+                    <div class="text-center mx-5 wow fadeIn" style="color: black !important;">
+                        <h1 class="mb-4" style="color: black !important;">
+                            <strong>Humanidad</strong>
+                        </h1>
+                        <p class="mb-4 d-none d-md-block" style="color: black !important;">
+                            <strong>El Movimiento Internacional de la Cruz Roja y de la Media Luna
+                                Roja, al que ha dado
+                                nacimiento la preocupación de prestar auxilio, sin discriminación, a
+                                todos los heridos en
+                                los campos de batalla, se esfuerza bajo su aspecto internacional y
+                                nacional, en prevenir y
+                                aliviar el sufrimiento de los hombres en todas las circunstancias.
+                                Tiende a proteger la vida
+                                y la salud, así como a hacer respetar a la persona humana. Favorece
+                                la comprensión mutua, la
+                                amistad, la cooperación y una paz duradera entre todos los
+                                pueblos.</strong>
+                        </p>
+                    </div>
+                    <!-- Content -->
+                </div>
+                <!-- Mask & flexbox options-->
+            </div>
+        </div>
+        <!--/Second slide-->
+        <!--Third slide-->
+        <div class="carousel-item">
+            <div class="view">
+                <!-- Mask & flexbox options-->
+                <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+                    <!-- Content -->
+                    <div class="text-center white-text mx-5 wow fadeIn">
+                        <h1 class="mb-4">
+                            <strong>Imparcialidad</strong>
+                        </h1>
+                        <p class="mb-4 d-none d-md-block">
+                            <strong>No hace ninguna distinción de nacionalidad, raza, religión,
+                                condición social ni credo
+                                político. Se dedica únicamente a socorrer a los individuos en
+                                proporción con los
+                                sufrimientos, remediando sus necesidades y dando prioridad a las más
+                                urgentes.</strong>
+                        </p>
+                    </div>
+                    <!-- Content -->
+                </div>
+                <!-- Mask & flexbox options-->
+            </div>
+        </div>
+        <!--/Third slide-->
+        <!--Fourth slide-->
+        <div class="carousel-item">
+            <div class="view">
+                <!-- Mask & flexbox options-->
+                <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+                    <!-- Content -->
+                    <div class="text-center white-text mx-5 wow fadeIn">
+                        <h1 class="mb-4">
+                            <strong>Neutralidad</strong>
+                        </h1>
+                        <p class="mb-4 d-none d-md-block">
+                            <strong>Con el fin de conservar la confianza de todos, se abstiene de
+                                tomar parte en las
+                                hostilidades y, en todo tiempo, en las controversias de orden
+                                político, racial, religioso e
+                                ideológico.</strong>
+                        </p>
+                    </div>
+                    <!-- Content -->
+                </div>
+                <!-- Mask & flexbox options-->
+            </div>
+        </div>
+        <!--/Fourth slide-->
+        <!--Five slide-->
+        <div class="carousel-item">
+            <div class="view">
+                <!-- Mask & flexbox options-->
+                <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+                    <!-- Content -->
+                    <div class="text-center white-text mx-5 wow fadeIn">
+                        <h1 class="mb-4">
+                            <strong>Independencia</strong>
+                        </h1>
+                        <p class="mb-4 d-none d-md-block">
+                            <strong>El Movimiento es independiente. Auxiliares de los poderes
+                                públicos en sus actividades
+                                humanitarias y sometidas a las leyes que rigen los países
+                                respectivos, las Sociedades
+                                Nacionales deben, sin embargo, conservar una autonomía que les
+                                permita actuar siempre de
+                                acuerdo con los principios de la Cruz Roja.</strong>
+                        </p>
+                    </div>
+                    <!-- Content -->
+                </div>
+                <!-- Mask & flexbox options-->
+            </div>
+        </div>
+        <!--/Five slide-->
+        <!--Six slide-->
+        <div class="carousel-item">
+            <div class="view">
+                <!-- Mask & flexbox options-->
+                <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+                    <!-- Content -->
+                    <div class="text-center white-text mx-5 wow fadeIn">
+                        <h1 class="mb-4">
+                            <strong>Voluntariado</strong>
+                        </h1>
+                        <p class="mb-4 d-none d-md-block">
+                            <strong>Es un Movimiento de socorro voluntario y de carácter
+                                desinteresado.</strong>
+                        </p>
+                    </div>
+                    <!-- Content -->
+                </div>
+                <!-- Mask & flexbox options-->
+            </div>
+        </div>
+        <!--Six slide-->
+        <div class="carousel-item">
+            <div class="view">
+                <!-- Mask & flexbox options-->
+                <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+                    <!-- Content -->
+                    <div class="text-center white-text mx-5 wow fadeIn">
+                        <h1 class="mb-4">
+                            <strong>Unidad</strong>
+                        </h1>
+                        <p class="mb-4 d-none d-md-block">
+                            <strong>En cada país solo puede existir una sola Sociedad de la Cruz
+                                Roja y de la Media Luna Roja,
+                                que debe ser accesible a todos y extender su acción humanitaria a la
+                                totalidad del
+                                territorio.</strong>
+                        </p>
+                    </div>
+                    <!-- Content -->
+                </div>
+                <!-- Mask & flexbox options-->
+            </div>
+        </div>
+        <!--/Six slide-->
+        <!--Seven slide-->
+        <div class="carousel-item">
+            <div class="view">
+                <!-- Mask & flexbox options-->
+                <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+                    <!-- Content -->
+                    <div class="text-center white-text mx-5 wow fadeIn">
+                        <h1 class="mb-4">
+                            <strong>Universalidad</strong>
+                        </h1>
+                        <p class="mb-4 d-none d-md-block">
+                            <strong>El Movimiento Internacional de la Cruz Roja y de la Media Luna
+                                Roja, en cuyo seno todas las
+                                Sociedades tienen los mismos derechos y el deber de ayudarse
+                                mutuamente, es universal.</strong>
+                        </p>
+                    </div>
+                    <!-- Content -->
+                </div>
+                <!-- Mask & flexbox options-->
+            </div>
+        </div>
+        <!--/Seven slide-->
+    </div>
+    <!--/.Slides-->
+    <!--Controls-->
+    <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev" id="btnPre">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next" id="btpPro">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+    <!--/.Controls-->
+</div>
